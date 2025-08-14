4f7365b76d0d7331d8feb969e913d59be11c618e
D <d@postmachina.ai>
2025-08-14 13:10:21 -0400

    Initial Commit

---
+++ GuillocheLogo/GuillocheLogoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuillocheLogoApp.swift
+//  GuillocheLogo
+//
+//  Created by Daren Smith on 8/14/25.
+//
+
+import SwiftUI
+
+@main
+struct GuillocheLogoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
